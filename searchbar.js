86f189fc35232a3d66cbb907d2ce9fc50ae82d5b
Viktor <viktorsink202@gmail.com>
2021-02-12 12:28:34 +0200

    Disappear bug with language bar was fixed

--- searchbar.js
+++ searchbar.js
@@ -2,29 +2,31 @@ $(document).ready(function() {
   var wrap = $(".header>.wrapper"),
       awrap = $(".header>.wrapper.active"),
       wndw = $(window);
+  const width = 800;
 
-
-
+  // search button listener
   $(".search.button").click(function(){
 
-    if(wndw.width() <= 537)
+    if(wndw.width() <= width + 37)
     {
         wrap.css({"display":"none"});
         awrap.css({"display":"flex"});
     }
   });
-
+  // cancel butoon listener
   $(".cancel.button").click(function(){
-    if(wndw.width() <= 537)
+    if(wndw.width() <= width + 37)
     {
         wrap.css({"display":"grid"});
         awrap.css({"display":"none"});
     }
   });
 
+  // resize window listener
   wndw.resize(function(){
 
-    if(wndw.width() > 537)
+    //change markup for 500+px screen width when search button is active
+    if(wndw.width() > width + 37)
     {
       wrap.css({"display":"grid"});
       awrap.css({"display":"none"});
